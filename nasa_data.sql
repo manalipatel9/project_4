f4011686f1fff9b95fc515eb839a7d04ca56f022
marybuie <maryelizabethbuie@gmail.com>
2023-12-10 21:26:32 -0600

    almost done

--- nasa_data.sql
+++ nasa_data.sql
@@ -1,14 +1,10 @@
 CREATE TABLE nasa (
-	index INT NOT NULL,
-    id INT   NOT NULL,
-    name VARCHAR NOT NULL,
-    est_diameter_min NUMERIC   NOT NULL,
-    est_diameter_max NUMERIC  NOT NULL,
-    relative_velocity NUMERIC   NOT NULL,
-    miss_distance NUMERIC  NOT NULL,
-    absolute_magnitude NUMERIC   NOT NULL,
-	hazardous BOOL NOT NUll, 
-	PRIMARY KEY (index)
+	"index" INT NOT NULL,
+    est_diameter_min NUMERIC NOT NULL,
+    est_diameter_max NUMERIC NOT NULL,
+    relative_velocity NUMERIC NOT NULL,
+    miss_distance NUMERIC NOT NULL,
+    absolute_magnitude NUMERIC NOT NULL,
+    hazardous VARCHAR NOT NULL,
+    PRIMARY KEY ("index")
 );
-
-select * from nasa 
